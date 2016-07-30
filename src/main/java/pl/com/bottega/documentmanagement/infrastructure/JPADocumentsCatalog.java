ae429003d0a2e8382f03fe3679165139cd50c890
Ulvarin <ulvarpl@gmail.com>
2016-07-30 13:33:11 +0200

    fx

--- src/main/java/pl/com/bottega/documentmanagement/infrastructure/JPADocumentsCatalog.java
+++ src/main/java/pl/com/bottega/documentmanagement/infrastructure/JPADocumentsCatalog.java
@@ -13,7 +13,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
 import java.util.Collection;
 import java.util.HashSet;
 
